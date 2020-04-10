6982bed9219a88bef4385b1fcb7ac15a44a0403f
Оскар Моисеев <blindjoedeath0@gmail.com>
2020-04-10 14:56:21 +0300

    Added simple no key hash generation

---
+++ RainbowCipher/HashGenerator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace RainbowCipher
+{
+    public class HashGenerator
+    {
+        private const int _blockLength = 16;
+        private ICipher _cipher;
+        private BlockSplitter _splitter = new BlockSplitter(_blockLength);
+        private byte[] _h0;
+
+        private void CreateH0()
+        {
+            _h0 = new byte[_blockLength];
+
+            var random = new Random();
+            random.NextBytes(_h0);
+        }
+
+        public HashGenerator(ICipher cipher)
+        {
+            CreateH0();
+            _cipher = cipher;
+        }
+
+        public byte[] Hash(byte[] data)
+        {
+            var blocks = _splitter.SplitOnBlocks(data);
+            var h = _h0;
+            foreach(var m in blocks)
+            {
+                var a = h;
+                var b = m.Xor(h);
+                var c = m.Xor(h);
+
+                _cipher.Key = a;
+                h = _cipher.Encrypt(b).Xor(c);
+            }
+            return h;
+        }
+    }
+}
--- RainbowCipher/Program.cs
+++ RainbowCipher/Program.cs
@@ -10,6 +10,16 @@ namespace RainbowCipher
     {
         static void Main(string[] args)
         {
+            var cipher = new Rainbow();
+            var hashGenerator = new HashGenerator(cipher);
+
+            var message = "Hello world!!!!!!:))))";
+            var messageData = Encoding.UTF8.GetBytes(message);
+            var resultData = hashGenerator.Hash(messageData);
+
+            
+            var result = Encoding.UTF8.GetString(resultData);
+            Console.WriteLine(result);
         }
     }
 }

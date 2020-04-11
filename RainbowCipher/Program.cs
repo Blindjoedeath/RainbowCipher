a738d049a356e4419132263f315fcc6ee920fd56
Оскар Моисеев <blindjoedeath0@gmail.com>
2020-04-11 15:31:50 +0300

    Cleaned main

--- RainbowCipher/Program.cs
+++ RainbowCipher/Program.cs
@@ -10,23 +10,6 @@ namespace RainbowCipher
     {
         static void Main(string[] args)
         {
-            bool isCorrect;
-            int count = 0;
-            do
-            {
-                var cipher = new Rainbow();
-                var hasher = new HashGenerator(cipher);
-                var signature = new CryptoSignature(hasher);
-
-                var random = new Random();
-                var messageLength = random.Next() % 100 + 50;
-                var message = new byte[messageLength];
-                random.NextBytes(message);
-
-                var sign = signature.CreateSignature(message);
-                isCorrect = signature.IsCorrect(sign);
-                Console.WriteLine($"Success {++count}");
-            } while (isCorrect);
         }
     }
 }
